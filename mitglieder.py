39e34b5eae530efdce6c235a1a40bd36bf04334b
Jan Henke <mail@taujhe.de>
2013-10-12 15:07:56 +0200

    Zwischenstand, Wahlreise einlesen.

---
+++ mitglieder.py
@@ -0,0 +1,93 @@
+#! /usr/bin/env python3
+"""Simples Skript für die Piratenpartei Deutschland Landesverband Hamburg
+Bestimmt den Wahlkreis für die Bezirkswahlen 2014."""
+
+import csv
+from re import match
+from os.path import join
+
+def enum(**enums):
+    return type('Enum', (), enums)
+
+Bezirke = enum(HamburgMitte = 1, Altona = 2, Eimsbuettel = 3, HamburgNord = 4, Wandsbek = 5, Bergedorf = 6, Harburg = 7, UNDEFINED = 0)
+Seite = enum(GERADE = 1, UNGREADE = 2)
+
+class Addresse(object):
+	"""Datenobjekt für eine Straße aus der Datendatei"""
+
+	class strassenSeite(object):
+		"""docstring for strassenSeite"""
+
+		bezirk = Bezirke.UNDEFINED
+		wahlkreisnummer = -1
+		wahlkreisname = ''
+
+		def __str__(self):
+			return "Bezirk: " + self.bezirk + ", Wahlkreis: " + self.wahlkreisnummer + " " + self.wahlkreisname
+
+	ungeradeSeite = strassenSeite()
+	geradeSeite = strassenSeite()
+
+	def setData(self, dataRow):
+		seite = self.strassenSeite()
+		seite.wahlkreisnummer = dataRow[5]
+		seite.wahlkreisname = dataRow[6]
+		bezirk = int(dataRow[3]) // 100
+		if bezirk == 1:
+			seite.bezirk = Bezirke.HamburgMitte
+		elif bezirk == 2:
+			seite.bezirk = Bezirke.Altona
+		elif bezirk == 3:
+			seite.bezirk = Bezirke.Eimsbuettel
+		elif bezirk == 4:
+			seite.bezirk = Bezirke.HamburgNord
+		elif bezirk == 5:
+			seite.bezirk = Bezirke.Wandsbek
+		elif bezirk == 6:
+			seite.bezirk = Bezirke.Bergedorf
+		elif bezirk == 7:
+			seite.bezirk = Bezirke.Harburg
+		nummern = dataRow[1].split()
+		if nummern[1] == '/':
+			m = match('(\d+)(\D+)', nummern[0])
+			if m:
+				start = nummern[0][:-1]
+			else:
+				start = nummern[0]
+			if (int(start) % 2) == 0:
+				self.geradeSeite = seite
+			else:
+				self.ungeradeSeite = seite
+		elif nummern[1] == '-':
+			self.ungeradeSeite = seite
+			self.geradeSeite = seite
+	
+	def __str__(self):
+		return "Gerade: " + self.geradeSeite + ", Ungerade: " + self.ungeradeSeite
+
+wahlkreisDaten = {'': Addresse()}
+			
+
+def readData(filename = join('data', 'Strassenverzeichnis_HH_insgesamt_BVWahl2014.csv')):
+	"""Datendatei lesen"""
+	with open(filename) as dataFile:
+		dataReader = csv.reader(dataFile, delimiter=';', quotechar='"')
+		for row in dataReader:
+			try:
+				adresse = wahlkreisDaten[row[0]]
+			except KeyError:
+				adresse = Addresse()
+				wahlkreisDaten[row[0]] = adresse
+			adresse.setData(row)
+	print(wahlkreisDaten)
+			
+def processMembers():
+	"Mitgliederdaten lesen und verarbeiten"
+	pass
+
+def main():
+	readData()
+	processMembers()
+
+if __name__ == '__main__':
+	main()
